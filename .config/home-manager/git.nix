f19a58b21d28e49cd1a2107e0682bfb0441dc058
Joseph DALY <joseph300905@gmail.com>
2023-08-04 01:51:49 +0200

    Basic Git config

---
+++ .config/home-manager/git.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+{
+  programs.git = {
+    enable = true;
+    userName = "Joseph DALY";
+    userEmail = "joseph300905@gmail.com";
+    extraConfig = {
+      init = {
+        defaultbranch = "main";
+      };
+    };
+  };
+}
