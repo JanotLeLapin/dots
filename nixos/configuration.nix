1e4dccb48ebde555b104381c53ec90c83f3e1773
Joseph DALY <joseph300905@gmail.com>
2023-08-04 01:30:08 +0200

    Grub + pretty theme

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -12,7 +12,23 @@
   ];
 
   # Bootloader
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub = {
+    enable = true;
+    useOSProber = true;
+    efiSupport = true;
+    device = "nodev";
+    theme = pkgs.stdenv.mkDerivation {
+      pname = "distro-grub-themes";
+      version = "3.1";
+      src = pkgs.fetchFromGitHub {
+        owner = "AdisonCavani";
+        repo = "distro-grub-themes";
+        rev = "v3.1";
+        hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+      };
+      installPhase = "cp -r customize/nixos $out";
+    };
+  };
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Enable networking
