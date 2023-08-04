61b56d2dfa30a62d0853c28f1901c718ab7c07a1
Joseph DALY <joseph300905@gmail.com>
2023-08-04 02:15:25 +0200

    Kitty

---
+++ .config/home-manager/kitty.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+{
+  programs.kitty = {
+    enable = true;
+    font = {
+      name = "Victor Mono";
+      size = 14;
+    };
+    theme = "Dark One Nuanced";
+    shellIntegration.enableZshIntegration = true;
+    settings = {
+      symbol_map = "U+f101-U+f208 nonicons";
+      disable_ligatures = "never";
+      window_padding_width = 4;
+      enable_audio_bell = "no";
+      confirm_os_window_close = 0;
+    };
+  };
+}
