ef747200884a1d9d6b02d6329e6dceaf5808f137
Joseph DALY <joseph300905@gmail.com>
2023-08-13 18:20:32 +0200

    Fixed setup script

--- setup.sh
+++ setup.sh
@@ -4,9 +4,12 @@ sudo nix-channel --add https://channels.nixos.org/nixos-unstable
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --add https://channels.nixos.org/nixos-unstable
 
+sudo nix-channel --update
+nix-channel --update
+
 # NixOS
 sudo rm -rf /etc/nixos
-sudo ln -s ./nixos /etc/nixos
+sudo ln -s $HOME/nixos /etc/nixos
 sudo nixos-generate-config
 sudo nixos-rebuild switch
 
