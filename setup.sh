786ccf0c6782253ec841f887994b400706edcedd
Joseph DALY <joseph300905@gmail.com>
2023-08-21 03:06:59 +0200

    Prevent build error

--- setup.sh
+++ setup.sh
@@ -2,18 +2,12 @@
 
 # Channels
 sudo nix-channel --add https://channels.nixos.org/nixos-unstable
-
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-nix-channel --add https://channels.nixos.org/nixos-unstable
-
+sudo nix-channel --add https://channels.nixos.org/nixos-unstable
 sudo nix-channel --update
-nix-channel --update
 
 # NixOS
 sudo rm -rf /etc/nixos
 sudo ln -s $HOME/nixos /etc/nixos
 sudo nixos-generate-config
-sudo nixos-rebuild switch
-
-# Home manager
-home-manager switch
+git add --intent-to-add $HOME/nixos
+sudo nixos-rebuild switch --impure
