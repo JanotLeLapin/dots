f928fd2ba5029e4d1eb6f977b47dbbd255741cd1
Joseph DALY <joseph300905@gmail.com>
2023-12-16 20:07:22 +0100

    fix unmatching versions

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,5 +1,12 @@
 {
-  inputs.hyprland.url = "github:hyprwm/Hyprland";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+    url = "github:nix-community/home-manager";
+    	inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland.url = "github:hyprwm/Hyprland";
+  };
 
   outputs = { nixpkgs, ... } @ inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
