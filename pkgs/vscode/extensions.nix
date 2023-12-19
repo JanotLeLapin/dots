31401fa81c0dac3ef6962d6042197cd7e64bc431
Joseph DALY <joseph300905@gmail.com>
2023-12-19 03:14:30 +0100

    better vscode integration

--- pkgs/vscode/extensions.nix
+++ pkgs/vscode/extensions.nix
@@ -1,4 +1,9 @@
 ext: with ext; [
   vscodevim.vim
+
   enkia.tokyo-night
+  vscode-icons-team.vscode-icons
+
+  jnoortheen.nix-ide
+  # rust-lang.rust-analyzer
 ]
--- pkgs/vscode/settings.nix
+++ pkgs/vscode/settings.nix
@@ -4,5 +4,6 @@
   "editor.fontLigatures" = true;
   "editor.fontSize" = 14;
   "workbench.colorTheme" = "Tokyo Night";
+  "workbench.iconTheme" = "vscode-icons";
   "window.zoomLevel" = 2;
 }
