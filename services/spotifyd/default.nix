22607834ffec811b85015e4ccc374b06125984ce
Joseph DALY <joseph300905@gmail.com>
2024-02-12 10:49:16 +0100

    avoid hardcoding runtime dir

--- services/spotifyd/default.nix
+++ services/spotifyd/default.nix
@@ -3,7 +3,7 @@
   settings = {
     global = {
       username = "31raeftls6no64md4eyuafy7cex4";
-      password = builtins.readFile ("/run/user/1000/secrets/password/spotify");
+      password_cmd = "cat $XDG_RUNTIME_DIR/secrets/password/spotify";
       device_name = "nix";
     };
   };
