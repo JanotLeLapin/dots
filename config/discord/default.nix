2c7a375c0d8c84c0b83c00c11c307ca4e62d0bcb
Joseph DALY <joseph300905@gmail.com>
2024-05-20 15:04:00 +0200

    remove discord theme

--- config/discord/default.nix
+++ config/discord/default.nix
@@ -1,9 +1,5 @@
-{ pkgs, ... }: let
-  theme = pkgs.fetchurl {
-    url = "https://refact0r.github.io/midnight-discord/midnight.css";
-    hash = "sha256-QIscjGYKNlCy1SyP+9mH3f8nCKO+Inj3NbT70WaR7SE=";
-  };
-in {
+{ pkgs, ... }:
+{
   text = builtins.toJSON {
     SKIP_HOST_UPDATE = true;
     IS_MAXIMIZED = true;
@@ -12,7 +8,6 @@ in {
       setup = true;
       cmdPreset = "battery";
       quickstart = true;
-      css = builtins.readFile theme;
     };
   };
   target = ".config/discord/settings.json";
