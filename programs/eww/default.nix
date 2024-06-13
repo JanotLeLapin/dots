421a192d60ac5ba520f5e177ad533b3996fec456
Joseph DALY <joseph300905@gmail.com>
2024-06-13 21:14:07 +0200

    use theme colors for eww

--- programs/eww/default.nix
+++ programs/eww/default.nix
@@ -1,4 +1,13 @@
-{ pkgs, ... }: {
+{ pkgs, specialArgs, ... }: {
   enable = true;
-  configDir = ./.;
+  configDir = pkgs.stdenv.mkDerivation {
+    name = "eww-config";
+    src = ./.;
+
+    installPhase = ''
+      mkdir $out
+      echo "${specialArgs.theme.format "#" ./eww.scss}" > $out/eww.scss
+      cp ./eww.yuck $out/eww.yuck
+    '';
+  };
 }
