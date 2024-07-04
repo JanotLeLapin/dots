c4cb31bbcfac019d7d71b5536169163d1cb59b17
Joseph DALY <joseph300905@gmail.com>
2024-07-04 17:17:28 +0200

    switch to floorp

--- home.nix
+++ home.nix
@@ -8,7 +8,7 @@ in {
     stateVersion = "23.05";
     packages = with pkgs; [
       nitch lsd mpc-cli # CLI tools
-      pcmanfm pavucontrol blueberry nicotine-plus deluge ario insomnia firefox # GUI
+      pcmanfm pavucontrol blueberry nicotine-plus qbittorrent ario insomnia floorp mullvad # GUI
       buildkit docker-compose # Docker
       tor-browser
     ] ++ listImport "pkgs" [ "jdtls" "gdlauncher" ];
