a58777ab663d64b3a8aa9a1bc7911d3455d285ce
Joseph DALY <joseph300905@gmail.com>
2024-07-06 20:01:08 +0200

    midnight discord theme

--- config/discord/default.nix
+++ config/discord/default.nix
@@ -1,5 +1,12 @@
-{ pkgs, ... }:
-{
+{ pkgs, ... }: let
+  theme = pkgs.fetchgit {
+    url = "https://github.com/refact0r/midnight-discord";
+    sparseCheckout = [
+      "flavors"
+    ];
+    hash = "sha256-a1rfJcVm4QE0+NR5BQGv7LtJFMiXNtEXaFyHenFw01s=";
+  };
+in {
   text = builtins.toJSON {
     SKIP_HOST_UPDATE = true;
     IS_MAXIMIZED = true;
@@ -8,6 +15,7 @@
       setup = true;
       cmdPreset = "battery";
       quickstart = true;
+      css = builtins.readFile "${theme}/flavors/midnight-rose-pine.theme.css";
     };
   };
   target = ".config/discord/settings.json";
