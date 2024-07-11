f1ed15750b9d01de774684ce3f01ae12b36ba9cd
Joseph DALY <joseph300905@gmail.com>
2024-07-11 11:58:32 +0200

    force full composition pipeline

--- gpu/nvidia.nix
+++ gpu/nvidia.nix
@@ -14,6 +14,7 @@
 
   hardware.nvidia = {
     modesetting.enable = true;
+    forceFullCompositionPipeline = !wayland;
     powerManagement.enable = false;
     powerManagement.finegrained = false;
     open = false;
