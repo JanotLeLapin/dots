cc91012b9dd677e0dc1b167f8e7a17ebfdc3b970
Joseph DALY <joseph300905@gmail.com>
2024-07-15 13:21:43 +0200

    dwm colors

--- dwm.h
+++ dwm.h
@@ -5,11 +5,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Victor Mono:size=10:style=Italic" };
 static const char dmenufont[]       = "Victor Mono:size=10:style=Italic";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#111827";
+static const char col_gray2[]       = "#1f2937";
+static const char col_gray3[]       = "#d1d5db";
+static const char col_gray4[]       = "#e5e7eb";
+static const char col_cyan[]        = "#818cf8";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
