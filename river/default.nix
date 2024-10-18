0bdcbbf5f27e6980670a6feb6c407b7f2a7a6a96
Joseph DALY <joseph300905@gmail.com>
2024-10-18 09:51:35 +0200

    river tag mappings

--- river/default.nix
+++ river/default.nix
@@ -1,13 +1,32 @@
 { pkgs, specialArgs, ... }: {
   enable = true;
   extraConfig = let
+    pow = x: y: if y == 0 then 1 else x * pow x (y - 1);
     touchpad = "$(riverctl list-inputs | grep Touchpad)";
+    tags = [
+      "ampersand"
+      "eacute"
+      "quotedbl"
+      "apostrophe"
+      "parenleft"
+      "minus"
+      "egrave"
+      "underscore"
+      "ccedilla"
+      "agrave"
+    ];
+    tagMappings = tag: key: ''
+      riverctl map normal Super ${key} set-focused-tags ${toString (pow 2 tag)}
+      riverctl map normal Super+Shift ${key} set-view-tags ${toString (pow 2 tag)}
+    '';
   in ''
     rivertile -view-padding 6 -outer-padding 6 &
     ${toString (map (monitor: "${pkgs.wlr-randr}/bin/wlr-randr --output ${monitor.name} --mode ${monitor.mode} --pos ${monitor.pos} --scale 1\n") specialArgs.monitors)}
 
     riverctl input ${touchpad} tap enabled
     riverctl input ${touchpad} natural-scroll enabled
+
+    ${pkgs.lib.strings.concatStrings (pkgs.lib.lists.imap1 tagMappings tags)}
   '';
   settings = {
     border-width = 2;
