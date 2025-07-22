56c8e9dc175da16485575f8ff65beab6e2dd6bf4
Joseph DALY <joseph300905@gmail.com>
2025-07-22 17:37:37 +0200

    configure st

--- home/default.nix
+++ home/default.nix
@@ -8,8 +8,18 @@ in {
     stateVersion = "25.05";
     packages = with pkgs; [
       nitch # cli
-      st pcmanfm pavucontrol blueberry floorp dmenu # gui
+      pcmanfm pavucontrol blueberry floorp dmenu # gui
       maim xclip # screenshot
+      (st.override {
+        conf = builtins.readFile ./st.h;
+        extraLibs = with pkgs; [ harfbuzz ];
+        patches = [
+          (pkgs.fetchurl {
+            url = "https://st.suckless.org/patches/ligatures/0.9/st-ligatures-20240105-0.9.diff";
+            hash = "sha256-Wcm2RTPlokGFlCsG/GmBEMrwNFpAxgEcqDaTEqKRhL0=";
+          })
+        ];
+      })
     ];
   };
 
