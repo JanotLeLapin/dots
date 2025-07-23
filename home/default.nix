ba31cf3fc5b012f658935545aee828363cfeb265
Joseph DALY <joseph300905@gmail.com>
2025-07-23 22:26:29 +0200

    use legcord instead of discord

--- home/default.nix
+++ home/default.nix
@@ -8,7 +8,7 @@ in {
     stateVersion = "25.05";
     packages = with pkgs; [
       nitch # cli
-      pcmanfm pavucontrol blueberry floorp dmenu bitwig-studio discord # gui
+      pcmanfm pavucontrol blueberry floorp dmenu bitwig-studio legcord # gui
       maim xclip # screenshot
       (st.override {
         conf = builtins.readFile ./st.h;
