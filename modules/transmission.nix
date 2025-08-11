6e69580447dc68caef4d7bc6ae441b4c8c4872fe
Joseph DALY <joseph300905@gmail.com>
2025-08-11 11:21:58 +0200

    make transmission directory writable for transmission group

--- modules/transmission.nix
+++ modules/transmission.nix
@@ -1,4 +1,15 @@
 { args, ... }: {
   services.transmission.enable = true;
   users.users."${args.user.name}".extraGroups = [ "transmission" ];
+  systemd.tmpfiles.settings = {
+    "10-transmission" = {
+      "/var/lib/transmission" = {
+        d = {
+          group = "transmission";
+          mode = "0775";
+          user = "transmission";
+        };
+      };
+    };
+  };
 }
